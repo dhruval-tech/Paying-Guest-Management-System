e6bd0c1af11b3fd6b23e9bfa3fd1b11c2e78e672
dhruval-tech <dhruvalgandhi2000@gmail.com>
2021-03-18 16:28:32 +0530

    Sign-in Feature

--- PG/Controllers/AccountController.cs
+++ PG/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PG.Data;
+using PG.ViewModels;
 
 namespace PG.Controllers
 {
@@ -25,19 +26,65 @@ namespace PG.Controllers
             _context = context;
 
         }
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+
+                else {
+                    ModelState.AddModelError("", "Invalid Login Attempt");
+                    return View();
+                }
+
+
+            }
+            return View(model);
+        }
+        [HttpGet]
         public IActionResult Register() {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var user = new IdentityUser { UserName = model.Email, Email = model.Email, EmailConfirmed = true };
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("index", "home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(model);
         }
         [HttpPost]
         public async Task<IActionResult> Logout()
         {
 
             await signInManager.SignOutAsync();
-            HttpContext.Session.Remove("Email");
             return RedirectToAction("index", "home");
         }
     }
